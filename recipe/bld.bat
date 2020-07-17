d64f89dd84511a5f0cc763995ad9b3d6246b7978
Angela Gloyna <angela.gloyna@gmail.com>
2020-07-17 18:49:31 -0500

    see if we can use wheel because long directory name issue with bazel

---
+++ recipe/bld.bat
@@ -0,0 +1,2 @@
+move tensorflow_probability-%PKG_VERSION%-py2.py3-none-any.whl.dummy tensorflow_probability-%PKG_VERSION%-py2.py3-none-any.whl
+%PYTHON% -m pip install --no-deps tensorflow_probability-%PKG_VERSION%-py2.py3-none-any.whl
